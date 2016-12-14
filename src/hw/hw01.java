92213a072e237eafedd299eaf25f6e773c13fd60
battlesushi <105021030@live.asia.edu.tw>
2016-12-14 17:14:54 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,30 @@
 package hw;
+import java.util.Scanner;
 /*
  * Date: 2016/12/12
- * Author: 105021***  ???
+ * Author: 105021030  張廷毓
  */
 public class hw01 {
 	public static void main(String[] args) {
-
+		Scanner sc=new Scanner(System.in);
+		String n=sc.nextLine();
+		String n2="";
+		int move=sc.nextInt();
+		int print;
+		for(int i=0;i<n.length()-1;i++){//轉密
+			if(n.charAt(i)!=' '){
+				print=(int)n.charAt(i);
+				n2=n2+""+(char)(print+move);
+			}
+			else if(n.charAt(i)==' ')
+				n2=n2+" ";
+			else
+				n2=n2+n.charAt(i);
+		}
+		for(int i=0;i<n.length()-1;i++){//顯示
+			System.out.print(n2.charAt(i));
+		}		
+			System.out.println();
+		
 	}
 }
